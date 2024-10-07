76e41b06867fb7df0a009e194a44e7dc5c44e857
dd <1109300975@qq.com>
2024-10-08 01:36:56 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+'''
+This is the code for baseline comparision of the Fashion-MNIST dataset.
+network architecture: 6-layer CNN with [input-128C3-AP2-256C3-AP2-256C3-AP2-512FC-10]
+'''
+
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
